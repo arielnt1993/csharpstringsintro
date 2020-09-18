734fa2df08504c6398cf985db4534e7df8fa3817
xteva <xteva@DESKTOP-4HEPKIM>
2020-09-18 02:18:52 -0300

    strings

---
+++ w3schoolLearning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace w3schoolLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("testing");
+            Console.Write("esto es un wirte no writeline");
+            Console.Write(" como podes ver");
+        }
+
+    }
+}
